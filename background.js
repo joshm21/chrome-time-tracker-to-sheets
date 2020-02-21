262a85ec04bacff552e9b464c91afb52a6e4f1fe
josh-meinders <44173063+josh-meinders@users.noreply.github.com>
2020-02-20 19:03:23 -0600

    Update background.js

--- background.js
+++ background.js
@@ -1,29 +1,28 @@
-// TABS
-// switch between tabs in window
-
-// close one of multiple tabs in window
-// close only tab in window
-// close background tab
-// close last tab
-// close all remaining tabs
-
-// open new tab and type url
-// update url in existing tab
-// open new background tab, switching to it after loaded
-// open new background tab, switching to it while loading
-
-// switch between windows without changing tabs
-// switch back to chrome from other app
-// chrome out of focus
-
-// idle
-
 const MINIMUM_SECONDS_THRESHOLD = 1;
-
 const DOMAINS_TO_NOT_SUBMIT = [null, "newtab"];
 
 let DOMAIN = null;
 let START_DATETIME = null;
+
+chrome.tabs.onActivated.addListener(handleEvent);
+chrome.tabs.onRemoved.addListener(handleEvent);
+chrome.tabs.onUpdated.addListener(handleEvent);
+chrome.windows.onFocusChanged.addListener(handleEvent);
+chrome.idle.onStateChanged.addListener(handleEvent);
+
+async function handleEvent() {
+  try {
+    const activeTab = await getSingleActiveTab();
+    const window = await getWindow(activeTab.windowId);
+    if (window.focused) {
+      processActiveTab(activeTab);
+    } else {
+      onNoActiveTab();
+    }
+  } catch (error) {
+    onNoActiveTab();
+  }
+}
 
 const getSingleActiveTab = () => {
   return new Promise(function(resolve, reject) {
@@ -51,31 +50,6 @@ const getWindow = windowId => {
   });
 };
 
-async function handleEvent() {
-  try {
-    const activeTab = await getSingleActiveTab();
-    const window = await getWindow(activeTab.windowId);
-    if (window.focused) {
-      processActiveTab(activeTab);
-    } else {
-      onNoActiveTab();
-    }
-  } catch (error) {
-    onNoActiveTab();
-  }
-}
-
-function getDomain(urlString) {
-  let domain;
-  try {
-    let url = new URL(urlString);
-    domain = url.hostname;
-  } catch (e) {
-    domain = null;
-  }
-  return domain;
-}
-
 function processActiveTab(activeTab) {
   let urlString = activeTab.url;
   let currentDomain = getDomain(urlString);
@@ -89,6 +63,17 @@ function processActiveTab(activeTab) {
 function onNoActiveTab() {
   submitData();
   DOMAIN = null;
+}
+
+function getDomain(urlString) {
+  let domain;
+  try {
+    let url = new URL(urlString);
+    domain = url.hostname;
+  } catch (e) {
+    domain = null;
+  }
+  return domain;
 }
 
 function submitData() {
@@ -106,9 +91,3 @@ function submitToSheets(domain, duration) {
   // let submitUrl = `https://docs.google.com/forms/d/e/1FAIpQLSecMPGUTWI7rinAmGfJSQE-Q13ZpPHLFSY4bL6Gvv8O7n2QxA/formResponse?&entry.607399626=${domain}&entry.1257183468=${duration}&submit=SUBMIT`;
   // fetch(submitUrl, { mode: "no-cors" });
 }
-
-chrome.tabs.onActivated.addListener(handleEvent);
-chrome.tabs.onRemoved.addListener(handleEvent);
-chrome.tabs.onUpdated.addListener(handleEvent);
-chrome.windows.onFocusChanged.addListener(handleEvent);
-chrome.idle.onStateChanged.addListener(handleEvent);
