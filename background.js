d3dedd3a803b01088bcd8e4d2590127caa5a60f5
joshm21 <44173063+joshm21@users.noreply.github.com>
2021-04-18 21:53:49 -0500

    Update background.js

--- background.js
+++ background.js
@@ -10,38 +10,63 @@ let ACTIVE_TAB = {
   }
 
 const pollActiveTab = async () => {
+  console.log("polling...")
   const state = await chrome.idle.queryState(SECONDS_UNTIL_IDLE)
-  switch (state) {
-    case "active":
-      const activeTab = await chrome.tabs.query({"active": true, "lastFocusedWindow": true})[0]
-      increment seconds or 
-      submit and update
-      break
+  const currentTab = await chrome.tabs.query({"active": true, "lastFocusedWindow": true})[0]
+  const hasActiveTabChanged = currentTab.title == ACTIVE_TAB.title ? true : false
 
-    case "idle":
-      // check for video
-      increment seconds or 
-      submit and reset
-      break
-    case "locked":
-      submitActiveTab()
-      resetActiveTab()
-      break
-    default:
-      break
+  if (state == "active" && hasActiveTabChanged) {
+    submitActiveTab()
+    ACTIVE_TAB.start = new Date()
+    ACTIVE_TAB.url = currentTab.url || currentTab.pendingUrl
+    ACTIVE_TAB.domain = new URL(ACTIVE_TAB.url).hostname
+    ACTIVE_TAB.title = currentTab.title
+    ACTIVE_TAB.seconds = 0
+    return
   }
+
+  if (state == "active" && !hasActiveTabChanged) {
+    ACTIVE_TAB.seconds += POLL_INTERVAL_SECONDS
+    return
+  }
+
+  if (state == "idle" && !hasActiveTabChanged) {
+    if (await chrome.scripting.executeScript(
+      {
+        target: {tabId: currentTab.id},
+        function: isVideoPlaying()
+      }
+    )) {
+      ACTIVE_TAB.seconds += POLL_INTERVAL_SECONDS
+    } else {
+      submitActiveTab()
+      ACTIVE_TAB.title = null
+    }
+    return
+  }
+
+  if (state == "locked") {
+    submitActiveTab()
+    ACTIVE_TAB.title = null
+    return
+  }
+
+  console.log("uncaught condition")
 }
-
-
 
 const submitActiveTab = () => {
-
+  console.log("submitting")
+  console.log(ACTIVE_TAB)
 }
 
-const resetActiveTab = () => {
-
+const isVideoPlaying = () => {
+  console.log("checking for running video")
+  const videoElement = document.getElementsByTagName('video')[0]
+  return (videoElement !== undefined && videoElement.currentTime > 0 && !videoElement.paused && !videoElement.ended && videoElement.readyState > 2)
 }
 
 while (true) {
-  pollActiveTab()
+  console.log("looping")
+  await pollActiveTab()
+  await new Promise(r => setTimeout(r, POLL_INTERVAL_SECONDS*1000));
 }
